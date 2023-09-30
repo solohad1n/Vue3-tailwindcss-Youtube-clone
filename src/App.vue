b68fb813af9f5f5db46be84702d2e3aeabaedd05
solohad1n <fox663399@gmail.com>
2023-09-30 15:18:33 +0600

    adding website layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <div class="text-green-500 text-2xl">
-      Test
-    </div>
-  </div>
+  <TheHeader />
+  <TheSidebarSmall />
+  <TheSidebar />
+  <TheSidebarMobile />
+  <TheCategories />
+  <TheVideos />
 </template>
 
-<style scoped>
-</style>
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheSidebarSmall from './components/TheSidebarSmall.vue'
+import TheSidebar from './components/TheSidebar.vue'
+import TheSidebarMobile from './components/TheSidebarMobile.vue'
+import TheCategories from './components/TheCategories.vue'
+import TheVideos from './components/TheVideos.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheSidebarSmall,
+    TheSidebar,
+    TheSidebarMobile,
+    TheCategories,
+    TheVideos
+  }
+}
+</script>
