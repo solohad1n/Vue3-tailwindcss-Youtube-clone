4d45d4cf9c627f61c7b343234479d42ddaa4dc0c
solohad1n <fox663399@gmail.com>
2023-10-20 15:47:36 +0600

    Animating dropdown lists

--- src/components/TheHeader/TheDropdownApps/TheDropdownApps.vue
+++ src/components/TheHeader/TheDropdownApps/TheDropdownApps.vue
@@ -1,29 +1,66 @@
 <template>
-  <div
-    class="opacity-0 group-hover:opacity-100 absolute top-9 right-0 sm:left-0 bg-white w-60 border border-t-0"
-  >
-    <section class="py-2 border-b">
-      <ul>
-        <DropdownAppsListItem label="YouTube TV"/>
-      </ul>
-    </section>
-    <section class="py-2 border-b">
-      <ul>
-        <DropdownAppsListItem label="YouTube Music"/>
-        <DropdownAppsListItem label="YouTube Kids"/>
-      </ul>
-    </section>
-    <section class="py-2">
-      <ul>
-        <DropdownAppsListItem label="Creator Academy"/>
-        <DropdownAppsListItem label="YouTube for Artists"/>
-      </ul>
-    </section>
+  <div class="relative" ref="elementOpen">
+    <button @click="isOpen = !isOpen" class="relative p-2 focus:outline-none">
+      <BaseIcon name="viewGrid" class="w-5 h-5"/>
+    </button>
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-from-class="transition opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <div
+        v-show="isOpen"
+        class="absolute top-9 right-0 sm:left-0 bg-white w-60 border border-t-0"
+      >
+        <section class="py-2 border-b">
+          <ul>
+            <DropdownAppsListItem label="YouTube TV"/>
+          </ul>
+        </section>
+        <section class="py-2 border-b">
+          <ul>
+            <DropdownAppsListItem label="YouTube Music"/>
+            <DropdownAppsListItem label="YouTube Kids"/>
+          </ul>
+        </section>
+        <section class="py-2">
+          <ul>
+            <DropdownAppsListItem label="Creator Academy"/>
+            <DropdownAppsListItem label="YouTube for Artists"/>
+          </ul>
+        </section>
+      </div>
+    </transition>
+
   </div>
 </template>
 
 <script setup>
 import DropdownAppsListItem from './DropdownAppsListItem.vue';
+import BaseIcon from '@/Icon/BaseIcon.vue'
+import { onMounted, ref } from 'vue';
+
+const isOpen = ref(false)
+const elementOpen = ref(null)
+
+onMounted(() => {
+  window.addEventListener('click', event => {
+    if(!event.composedPath().includes(elementOpen.value)) {
+      isOpen.value = false
+    }
+  })
+})
+
+// onMounted(() => {
+//   window.addEventListener('click', event => {
+//     if (!elementOpen.value.contains(event.target)) {
+//       isOpen.value = false
+//     }
+//   })
+// })
 </script>
 
 <style>
--- src/components/TheHeader/TheDropdownSettings/TheDropdownSettings.vue
+++ src/components/TheHeader/TheDropdownSettings/TheDropdownSettings.vue
@@ -1,31 +1,59 @@
 <template>
-  <div
-    class="opacity-0 group-hover:opacity-100 absolute top-9 -right-full sm:right-0 bg-white w-72 border border-t-0"
-  >
-    <section class="py-2 border-b">
-      <ul>
-        <DropdownSettingsListItem
-        v-for="listItem in listItems.slice(0, 8)"
-        :key="listItem.label"
-        :label="listItem.label"
-        :icon="listItem.icon"
-        :withSubMenu="listItem.withSubMenu"
-        />
-      </ul>
-    </section>
-    <section class="py-2">
-      <ul>
-        <DropdownSettingsListItem
-        :label="listItems[8].label"
-        :withSubMenu="listItems[8].withSubMenu"
-        />
-      </ul>
-    </section>
+  <div class="relative" ref="elementOpen">
+    <button class="relative p-2 focus:outline-none" @click="isOpen = !isOpen">
+      <BaseIcon name="dotsVertical" class="w-5 h-5"/>
+    </button>
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-from-class="transition opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <div
+        class="absolute top-9 -right-full sm:right-0 bg-white w-72 border border-t-0"
+        v-show="isOpen"
+      >
+        <section class="py-2 border-b">
+          <ul>
+            <DropdownSettingsListItem
+            v-for="listItem in listItems.slice(0, 8)"
+            :key="listItem.label"
+            :label="listItem.label"
+            :icon="listItem.icon"
+            :withSubMenu="listItem.withSubMenu"
+            />
+          </ul>
+        </section>
+        <section class="py-2">
+          <ul>
+            <DropdownSettingsListItem
+            :label="listItems[8].label"
+            :withSubMenu="listItems[8].withSubMenu"
+            />
+          </ul>
+        </section>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup>
 import DropdownSettingsListItem from './DropdownSettingsListItem.vue';
+import BaseIcon from '@/Icon/BaseIcon.vue'
+import { onMounted, ref } from 'vue';
+
+const isOpen = ref(false)
+const elementOpen = ref(null)
+
+onMounted(() => {
+  window.addEventListener('click', event => {
+    if (!elementOpen.value.contains(event.target)) {
+      isOpen.value = false
+    }
+  })
+})
 
 const listItems = [
   {
